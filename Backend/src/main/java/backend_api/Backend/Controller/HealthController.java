80ede40b1bee062982902173b731d64f2c2187b0
Lorenzo <lorenzo@Lorenzos-MacBook-Air.local>
2025-09-09 18:20:46 -0300

    Demostracion

--- Backend/src/main/java/backend_api/Backend/Controller/HealthController.java
+++ Backend/src/main/java/backend_api/Backend/Controller/HealthController.java
@@ -9,7 +9,7 @@ public class HealthController {
 
     @GetMapping("/health")
     public ResponseEntity<String> health() {
-        return ResponseEntity.ok("UP 4");
+        return ResponseEntity.ok("UP Lucas");
     }
 }
 
