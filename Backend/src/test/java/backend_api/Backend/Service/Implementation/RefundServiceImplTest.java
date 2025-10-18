7a03dbcaddc5dba17e02099913d8dbeda654e8c1
Edgardo Crespo <ecrespo@decampoacampo.dev>
2025-10-18 15:39:50 -0300

    fail refund

---
+++ Backend/src/test/java/backend_api/Backend/Service/Implementation/RefundServiceImplTest.java
@@ -0,0 +1,4 @@
+package backend_api.Backend.Service.Implementation;
+
+// This test file has been removed as RefundServiceImpl was removed
+// The RefundServiceImpl functionality is no longer needed
